1c92d8a8919587ad3da40521af7c05757fbe548c
Shigeki-Kamimura <kamimura@nextreservation.jp>
2022-08-30 13:38:53 +0900

    初回上流コミット

---
+++ header.php
@@ -0,0 +1,35 @@
+<header id="fixedHeader">
+<div>
+	<a href="<?= $base ?>" class="site_name">
+		<img src="<?= $base ?>logo-black.png" alt="有限会社TRO" class="logo-small">
+		<img src="<?= $base ?>logo-black.png" alt="有限会社TRO" class="logo-large">
+	</a>
+	<!-- <div class="hamburger_icon" id="hamburgerIcon">MENU</div>
+	<div class="hamburger_nav" id="hamburgerNav">
+	<ul>
+		<li><a href="<?= $base ?>">ユニットハウス</br>レンタル・販売</a></li>
+		<li><a href="<?= $base ?>used/">中古事務用品</br>レンタル・販売</a></li>
+		<li><a href="<?= $base ?>signboard/">看板制作</a></li>
+		<li><a href="<?= $base ?>cleaning/">お部屋・料理</a></li>
+		<li><a href="<?= $base ?>town_fes/">地鎮祭</a></li>
+		<li><a href="<?= $base ?>about/">会社概要</a></li>
+	</ul> -->
+	<a class="tel" href=""><img src="" alt=""></a>
+</div>
+
+<div>
+<ul class="aside_nav">
+	<li><a href="<?= $base ?>contact/">お問い合わせ</a></li>
+</ul>
+<nav class="global_nav">
+	<ul>
+		<li><a href="<?= $base ?>">ユニットハウス</br>レンタル・販売</a></li>
+		<li><a href="<?= $base ?>used/">中古事務用品</br>レンタル・販売</a></li>
+		<li><a href="<?= $base ?>signboard/">看板制作</a></li>
+		<li><a href="<?= $base ?>cleaning/">お部屋・料理</a></li>
+		<li><a href="<?= $base ?>town_fes/">地鎮祭</a></li>
+		<li><a href="<?= $base ?>about/">会社概要</a></li>
+	</ul>
+</nav>
+</div>
+</header>
