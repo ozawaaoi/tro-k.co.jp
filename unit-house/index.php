498dee13cf8a0c0f8863a45259768afb3b0db23f
ozawaaoi <blue.aoringo7@gmail.com>
2022-08-30 19:19:41 +0900

    ボタン作成

--- unit-house/index.php
+++ unit-house/index.php
@@ -43,7 +43,11 @@ $base = '../';
 			<div>
 				<p>ユニットハウスというと建設現場に使われることが多いイメージだが、つぎのような様々な用途にお使いいただけるものを用意していること。</p>
 			</div>
-			<a href="" target="_blank">
+			<a href="" target="_blank" class="btu">
+				三協フロンテア公式サイトへ
+				<img src="../arrow-1.svg" alt="">
+			</a>
+			<a href="" target="_blank" class="btn_1">
 				三協フロンテア公式サイトへ
 				<img src="" alt="">
 			</a>
