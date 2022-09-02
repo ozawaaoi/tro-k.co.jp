48da1509f9916e880cce3df01facc4e486732784
Shigeki-Kamimura <kamimura@nextreservation.jp>
2022-09-02 17:14:21 +0900

    headerの作成(レスポンシブ対応)

--- header.php
+++ header.php
@@ -3,20 +3,21 @@
 	<div>
 		<div class="site_name_wrapper">
 			<a href="<?= $base ?>" class="site_name">
-				<img src="<?= $base ?>logo-black.png" alt="TRO" class="logo-black">
-				<span>有限会社TRO</span>
+				<img src="<?= $base ?>logo-yellow.png" alt="TRO" class="logo-yellow">
+				<span>有限会社トロ</span>
 			</a>
 		</div>
 				<!-- hamburger_menu -->
 		<div class="hamburger_icon" id="hamburgerIcon">MENU</div>
 		<div class="hamburger_nav" id="hamburgerNav">
 			<ul>
-				<li><a href="<?= $base ?>">ユニットハウス</br>レンタル・販売</a></li>
+				<li><a href="<?= $base ?>unit-house/">ユニットハウス</br>レンタル・販売</a></li>
 				<li><a href="<?= $base ?>used/">中古事務用品</br>レンタル・販売</a></li>
 				<li><a href="<?= $base ?>signboard/">看板制作</a></li>
-				<li><a href="<?= $base ?>cleaning/">お部屋・料理</a></li>
+				<li><a href="<?= $base ?>cleaning/">お仏壇クリーニング</a></li>
 				<li><a href="<?= $base ?>town_fes/">地鎮祭</a></li>
 				<li><a href="<?= $base ?>about/">会社概要</a></li>
+				<li><a href="<?= $base ?>about/">採用情報</a></li>
 			</ul>
 		<a class="tel" href=""><img src="" alt=""></a>
 		</div> <!-- end of hamburger_menu -->
@@ -25,12 +26,13 @@
 		<div class="global_nav_wrapper">
 			<nav class="global_nav">
 				<ul>
-					<li><a href="<?= $base ?>">ユニットハウス</br>レンタル・販売</a></li>
+					<li><a href="<?= $base ?>unit-house/">ユニットハウス</br>レンタル・販売</a></li>
 					<li><a href="<?= $base ?>used/">中古事務用品</br>レンタル・販売</a></li>
 					<li><a href="<?= $base ?>signboard/">看板制作</a></li>
-					<li><a href="<?= $base ?>cleaning/">お部屋・料理</a></li>
+					<li><a href="<?= $base ?>cleaning/">お仏壇<br>クリーニング</a></li>
 					<li><a href="<?= $base ?>town_fes/">地鎮祭</a></li>
 					<li><a href="<?= $base ?>about/">会社概要</a></li>
+					<li><a href="<?= $base ?>about/">採用情報</a></li>
 				</ul>
 			</nav>
 		</div><!-- end of global nav -->
